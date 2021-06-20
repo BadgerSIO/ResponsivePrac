702d80c5a6e7b81fe6a3177ba9578f9f30770177
Fowzia Sultana <mzia16813@gmail.com>
2021-06-20 17:09:42 +0600

    Second commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#banner').slick({
+        arrows: false,
+//         slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+//   speed:2000
+    });
+
+});
